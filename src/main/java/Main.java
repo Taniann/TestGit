9125b4eb89adcfde31b44073d9a210cfed56688b
Таня <nebesnatania@outlook.com>
2018-12-27 19:58:06 +0200

    first
    (cherry picked from commit 71183cf)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world, changes in branch3");
+        System.out.println("hello world");
+        System.out.println("Branch 2 first commit");
     }
 }
