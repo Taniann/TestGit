c8940e4d90d1a1082f21854f3a36acab8fe2fbb1
Таня <nebesnatania@outlook.com>
2018-12-27 19:58:48 +0200

    second

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("Branch 2 first commit");
+        System.out.println("Branch 2 second commit ");
     }
 }
